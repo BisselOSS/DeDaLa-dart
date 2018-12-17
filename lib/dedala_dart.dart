d0bd78582e77344723d495315d17d98f52219b3e
Julian Bissekkou <julianbissekkou@quickbirdstudios.com>
2018-12-17 13:40:29 +0100

    master: export classes needed for using this libary.

--- lib/dedala_dart.dart
+++ lib/dedala_dart.dart
@@ -1,6 +1,9 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library dedala_dart;
 
-export 'package:dedala_dart/dedala_dart.dart';
+export 'package:dedala_dart/src/cache.dart';
+export 'package:dedala_dart/src/caches/memory_cache.dart';
+export 'package:dedala_dart/src/compose/cache_connection_controller.dart';
+export 'package:dedala_dart/src/de_da_la.dart';
+export 'package:dedala_dart/src/optional.dart';
+export 'package:dedala_dart/src/policy/insert_policy.dart';
+export 'package:dedala_dart/src/policy/read_policy.dart';
